88955ca8002d5b12f9ef1627292618d65fb682e8
GustavoCasco <gugugacasco007@gmail.com>
2020-09-11 10:26:48 -0300

    Atualização base API

--- Backend/ProVagas.WebApi/ProVagas.WebApi/LoginViewModel/Login.cs
+++ Backend/ProVagas.WebApi/ProVagas.WebApi/LoginViewModel/Login.cs
@@ -3,8 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
-using Xunit;
-using Xunit.Sdk;
+
 
 namespace ProVagas.LoginViewModel
 {
