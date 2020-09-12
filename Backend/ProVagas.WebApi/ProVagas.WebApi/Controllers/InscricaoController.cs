e90e3d7df3bd7e99e2d65164b7d6678a65fac7c9
Flavio Pires <fdnpires@gmail.com>
2020-09-12 16:03:13 -0300

    Criado as interfaces, repositories e controllers das tabelas Candidato, Inscricao e StatusInscricao

---
+++ Backend/ProVagas.WebApi/ProVagas.WebApi/Controllers/InscricaoController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProVagas.WebApi.Interfaces;
+using ProVagas.WebApi.Repositories;
+
+namespace ProVagas.WebApi.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InscricaoController : ControllerBase
+    {
+        private IInscricaoRepository _inscricaoRepository;
+
+        public InscricaoController()
+        {
+            _inscricaoRepository = new InscricaoRepository();
+        }
+
+
+    }
+}
---
+++ Backend/ProVagas.WebApi/ProVagas.WebApi/Interfaces/IStatusInscricaoRepository.cs
@@ -0,0 +1,32 @@
+﻿using ProVagas.WebApi.Domains;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProVagas.WebApi.Interfaces
+{
+    /// <summary>
+    /// Interface responsável pelo StatusInscricaoRespository
+    /// </summary>
+    interface IStatusInscricaoRepository
+    {
+        /// <summary>
+        /// Lista todos os status de inscrição
+        /// </summary>
+        /// <returns>Uma lista de status de inscrição</returns>
+        List<StatusInscricao> Listar();
+
+        /// <summary>
+        /// Cadastra um novo Status de inscrição
+        /// </summary>
+        /// <param name="novoStatusInscricao">Objeto contendo as informações do novo status de inscrição</param>
+        void Cadastrar(StatusInscricao novoStatusInscricao);
+
+        /// <summary>
+        /// Deleta um status de inscrição existente
+        /// </summary>
+        /// <param name="id">Id do status de inscrição que será deletado</param>
+        void Deletar(int id);
+    }
+}
