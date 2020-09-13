cee2d42611d8600a81d33345b7c61276bf79502b
Nakkat <53802334+Nakkat@users.noreply.github.com>
2020-09-13 11:49:21 -0300

    Atualização Startup e Domain / CRUD Estagio / Empresa / Beneficio

---
+++ Backend/ProVagas/Interfaces/IEstagioRepository.cs
@@ -0,0 +1,21 @@
+﻿using ProVagas.Domains;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProVagas.Interfaces
+{
+    interface IEstagioRepository
+    {
+        List<Estagio> Listar();
+
+        void Cadastrar(Estagio novoEstagio);
+
+        void Atualizar(int id, Estagio estagioAtualizado);
+
+        void Deletar(int id);
+
+        Estagio BuscarPorId(int id);
+    }
+}
