1a006d6bfad654e982603008b526b561184fbb63
Gustavo Casco Zacharias <51628550813@info.local>
2020-09-17 10:28:47 -0300

    CRUD COMPLETO

---
+++ Backend/ProVagas/Interfaces/ICidadeRepository.cs
@@ -0,0 +1,18 @@
+﻿using ProVagas.Domains;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProVagas.Interfaces
+{
+    interface ICidadeRepository
+    {
+        List<Cidade> Listar();
+
+        void Cadastrar(Cidade novacidade);
+
+        void Atualizar(int id, Cidade cidadeAtualizada);
+        void Deletar(int id);
+    }
+}
