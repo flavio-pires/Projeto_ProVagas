4008e8e9fb38f7e2bf129543e338f404a8ae8d79
Flavio Pires <fdnpires@gmail.com>
2020-10-21 17:08:19 -0300

    Pastas criadas, rotas adicionadas e componentes Button e Input

---
+++ Frontend/React/provagas/src/pages/Home/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+function Home(){
+    return(
+        <div className="centro">
+            <div className="home">
+                <h1>Pro Vagas</h1>
+            </div>
+        </div>
+    );
+}
+
+export default Home;
