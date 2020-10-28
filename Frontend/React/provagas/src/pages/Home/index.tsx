c4fedb2fcb8a0952101868e20901a546fc5c6bdd
Daniel Mesquita <danielbmesquita@gmail.com>
2020-10-28 20:34:40 -0300

    adição do header

--- Frontend/React/provagas/src/pages/Home/index.tsx
+++ Frontend/React/provagas/src/pages/Home/index.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import Footer from '../../components/Footer';
+import Header from '../../components/Header';
 
 
 function Home(){
     return(
         <div className="centro">
             <div className="home">
-                <h1>Pro Vagas</h1>
-                <p>oi</p>
+                <Header/>
                 <Footer/>
             </div>
         </div>
