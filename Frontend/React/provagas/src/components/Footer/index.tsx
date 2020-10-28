63ae9ac432f614eda4b7829b88208be00dade26b
Daniel Mesquita <danielbmesquita@gmail.com>
2020-10-28 19:52:30 -0300

    criação e organização do Footer

--- Frontend/React/provagas/src/components/Footer/index.tsx
+++ Frontend/React/provagas/src/components/Footer/index.tsx
@@ -12,17 +12,22 @@ import './style.css';
 function Footer() {
   return (
     <div className="footer">
-      <img src={logosenai} alt="Logotipo do Senai, vermelho e branco." />
-      <div>
-        <p>Escola SENAI de informatica</p>
-        <p>Avenida Alameda Barâo de Limeira</p>
-        <p>CEP: 01202-001</p>
-        <p>Telefone: (11)3273-5000</p>
+      <div className="logoFooter">
+        <img src={logosenai} alt="Logotipo do Senai, vermelho e branco." />
+        <hr/>
+          <div>
+            <p>Escola SENAI de informatica</p>
+            <p>Avenida Alameda Barâo de Limeira</p>
+            <p>CEP: 01202-001</p>
+            <p>Telefone: (11)3273-5000</p>
+          </div>
       </div>
-      <img src={facebook} alt="Logotipo facebook, com link direto para o facebook." />
-      <img src={instagram} alt="Logotipo instagram, com link direto para o instagram." />
-      <img src={twitter} alt="Logotipo twitter, com link direto para o twitter." />
-      <img src={linkedin} alt="Logotipo linkedin, com link direto para o linkedin." />
+      <div className="iconsFooter">
+        <img src={facebook} alt="Logotipo facebook, com link direto para o facebook." />
+        <img src={instagram} alt="Logotipo instagram, com link direto para o instagram." />
+        <img src={twitter} alt="Logotipo twitter, com link direto para o twitter." />
+        <img src={linkedin} alt="Logotipo linkedin, com link direto para o linkedin." />
+      </div>
     </div>
   );
 }
