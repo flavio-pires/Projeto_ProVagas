c9374d9f633997510d4508eacc81aacece240422
Carllotta <carllottadias@gmail.com>
2020-11-05 16:18:39 -0300

    Adicionando rota da tela de tipoUsuario

--- Frontend/React/provagas/src/routers.tsx
+++ Frontend/React/provagas/src/routers.tsx
@@ -2,13 +2,14 @@ import React from 'react';
 import {BrowserRouter, Route} from 'react-router-dom';
 import Home from './pages/Home';
 import Cadastro from './pages/Cadastro';
+import TipoCadastro from './pages/TipoCadastro';
 
 function Routers() {
     return(
         <BrowserRouter>
         <Route path="/" exact component={Home}/>
         <Route path="/cadastro" component={Cadastro}/>
-        
+        <Route path="/tipoCadastro" component={TipoCadastro}/>
         </BrowserRouter>
     );
 }
