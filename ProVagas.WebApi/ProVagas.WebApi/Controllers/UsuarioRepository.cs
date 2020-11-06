b0a131fc9204a829a5b05478be9fcfd60d13b2f7
Daniel Mesquita <danielbmesquita@gmail.com>
2020-11-06 02:06:34 -0300

    correção de dados

---
+++ ProVagas.WebApi/ProVagas.WebApi/Controllers/UsuarioRepository.cs
@@ -0,0 +1,45 @@
+﻿using ProVagas.WebApi.Domains;
+using ProVagas.WebApi.Interfaces;
+using ProVagas.WebApi.ViewsModels;
+using System.Collections.Generic;
+
+namespace ProVagas.WebApi.Controllers
+{
+    internal class UsuarioRepository : IUsuarioRepository
+    {
+        public void Add(Usuario obj)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Usuario BuscarSenhaEmail(string email, string senha)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool CadastrarAdm(CadastrarAdmViewModels cadastrarAdmViewModels)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Delete(Usuario obj)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerable<Usuario> GetAll()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Usuario GetById(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Update(Usuario obj)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
